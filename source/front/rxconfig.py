b83edab9d332fa0c41360f2f838cc77fda5b4b87
Hu9o73 <hbonnell@live.fr>
2025-12-20 15:33:34 +0100

    feat(app): Tweaks for prod

--- source/front/rxconfig.py
+++ source/front/rxconfig.py
@@ -4,10 +4,8 @@ import reflex as rx
 
 config = rx.Config(
     app_name="chatakon_front",
-    api_url=os.getenv("REFLEX_API_URL", "http://localhost:8000"),
-    frontend_port=3000,
-    backend_port=8000,
-    frontend_host="0.0.0.0",
-    backend_host="0.0.0.0",
+    api_url="http://chatakon-agentic:8001",
+    api_base_url = os.getenv("BACKEND_URL"),
+    cors_allowed_origins=["http://localhost:3000", "http://localhost:8091", "https://chatakon.fr"],
     show_built_with_reflex=False,
 )
