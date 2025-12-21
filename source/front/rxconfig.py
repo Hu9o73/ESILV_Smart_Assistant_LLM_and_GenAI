a971f4310a1cb38e7a89f4567e717e46a69ea638
Hu9o73 <hbonnell@live.fr>
2025-12-21 18:09:25 +0100

    task(front): Update rxconfig

--- source/front/rxconfig.py
+++ source/front/rxconfig.py
@@ -5,7 +5,6 @@ import reflex as rx
 config = rx.Config(
     app_name="chatakon_front",
     agentic_api_url="http://chatakon-agentic:8001",
-    api_base_url = os.getenv("BACKEND_URL"),
     cors_allowed_origins=["http://localhost:3000", "http://localhost:8091", "https://chatakon.fr"],
     show_built_with_reflex=False,
 )
